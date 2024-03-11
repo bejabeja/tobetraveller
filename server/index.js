2e89aa002b1bac10d8fa57851922c107ceb7ef3d
bejabeja <miriam.abella211@gmail.com>
2024-03-11 11:46:13 +0100

    add config vercel, cors and bodyparser

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(express.json())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const corsOptions = {
+    origin: '*',
+    credential: true,
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions))
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server Mimi!" });
   });
-  
 
+  
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
