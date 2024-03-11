933aae309c939f6b0ecd655db9498d88d27fbc6d
bejabeja <miriam.abella211@gmail.com>
2024-03-11 13:23:47 +0100

    Install axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,39 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState([]);
+  const [placeData, setPlaceData] = useState([]);
+
 
   useEffect(() => {
     let apiUrl = process.env.REACT_APP_API_URL;
 
+    let processing = true 
+    axiosFetchData(processing, apiUrl)
+    return() => {
+      processing = false
+    }
+  }, [])
 
-    fetch(`${apiUrl}/places`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return res.json();
-      })
-      .then(res => {
-        setData(res);
-      })
-      .catch(err => console.log(err)) 
-  }, []);
+  const axiosFetchData = async(processing, apiUrl) => {
+    await axios.get(`${apiUrl}/places`)
+    .then(res => {
+      if(processing){
+        setPlaceData(res.data)
+      }
+    })
+    .catch(err => console.log(err))
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {data ? (
+        {placeData ? (
           <div>
-          {data.map((item) => (
+          {placeData.map((item) => (
             <div key={item.id}>
               <p>{item.id}</p>
               <p>{item.cityName}</p>
