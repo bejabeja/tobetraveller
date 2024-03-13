7525d172fac712f9343f8641307523938f3ca59d
bejabeja <miriam.abella211@gmail.com>
2024-03-13 17:24:44 +0100

    select navbar icon when is in the page

--- client/src/navbar/Navbar.js
+++ client/src/navbar/Navbar.js
@@ -5,17 +5,29 @@ import TravelIcon from '../icons/TravelIcon'
 import WeatherIcon from '../icons/WeatherIcon'
 import ProfileIcon from '../icons/ProfileIcon'
 import ShareIcon from '../icons/ShareIcon'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+  const location = useLocation()
   return (
     <div className='app-navbar'>
         <nav className='navbar-list'>
           <ul className='navbar-links'>
-            <li><a href="/"><HomeIcon></HomeIcon></a></li>
-            <li><a href="/discover"><TravelIcon></TravelIcon></a></li>
-            <li><a href="/weather"><WeatherIcon></WeatherIcon></a></li>
-            <li><a href="/share"><ShareIcon></ShareIcon></a></li>
-            <li><a href="/profile"><ProfileIcon></ProfileIcon></a></li>
+            <li className={location.pathname === '/' ? 'navbar-link--active' : 'navbar-link'}>
+              <a href="/"><HomeIcon></HomeIcon></a>
+            </li>
+            <li className={location.pathname === '/discover' ? 'navbar-link--active' : 'navbar-link'}>
+              <a href="/discover"><TravelIcon></TravelIcon></a>
+            </li>
+            <li className={location.pathname === '/weather' ? 'navbar-link--active' : 'navbar-link'}>
+              <a href="/weather"><WeatherIcon></WeatherIcon></a>
+            </li>
+            <li className={location.pathname === '/share' ? 'navbar-link--active' : 'navbar-link'}>
+              <a href="/share"><ShareIcon></ShareIcon></a>
+            </li>
+            <li className={location.pathname === '/profile' ? 'navbar-link--active' : 'navbar-link'}>
+              <a href="/profile"><ProfileIcon></ProfileIcon></a>
+            </li>
           </ul>
         </nav>
       </div>
