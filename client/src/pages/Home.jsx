ffd1d6a8f8d381d748ae05d28d7cb79b262882d3
bejabeja <miriam.abella211@gmail.com>
2024-04-16 15:51:06 +0200

    add logo to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import './Home.css'
 import ButtonLink from '../components/ButtonLink'
 import Layout from '../layout/Layout'
+import logo from '../logos/tobetraveller3black.png'
 
 const Home = () => {
 
   return (
     < Layout>
-      <section className='tittle-group section'>
-        <h1 className='title--h1'>Travel Smarter,</h1>
+      <main className='home'>
+        <a href='/discover'>
+          <img src={logo}></img>
+        </a>
+        {/* <h1 className='title--h1'>Travel Smarter,</h1>
         <h1 className='title--h1'> Dream Bigger</h1>
 
-        <h2 className='title--h2'>Your Journey Begins Here</h2>
-        <ButtonLink href='/discover' className='main--button' text='Start'> </ButtonLink>
-      </section>
+        <h2 className='title--h2'>Your Journey Begins Here</h2> */}
+        {/* <ButtonLink href='/discover' className='main--button' text='Start'> </ButtonLink> */}
+      </main>
     </ Layout>
   )
 }
