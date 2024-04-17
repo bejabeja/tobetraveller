8643ddc95d61895e7d4cb8e7cda44987cb544a4c
bejabeja <miriam.abella211@gmail.com>
2024-04-17 20:38:33 +0200

    styling create trip

--- client/src/pages/trip/CreateTrip.jsx
+++ client/src/pages/trip/CreateTrip.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import './CreateTrip.css'
 import ButtonLink from "../../components/ButtonLink";
+import logo from "../../logos/tobetraveller3black.png"
 
 const CreateTrip = (props) => {
     const [newTrip, setNewTrip] = useState(false)
     const [allTrips, setAllTrips] = useState(false)
 
-    const [tripInfo, setTripInfo] = useState({ placeToGo: '', travelDays: '' })
+    const [tripInfo, setTripInfo] = useState({ placeToGo: '', travelDays: '', headerImg: null })
     const { placeToGo, travelDays } = tripInfo
 
     async function handleNewTrip(e) {
@@ -15,6 +16,24 @@ const CreateTrip = (props) => {
         setNewTrip(true)
     }
 
+    function handleHeaderImageChange(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setTripInfo({ ...tripInfo, headerImg: reader.result })
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+    const handleAddImgButtonClick = () => {
+        document.getElementById('fileInput').click();
+    };
+
+    const handleRemoveImgButtonClick = () => {
+        setTripInfo({ ...tripInfo, headerImg: null })
+    }
 
     const renderTripDays = () => {
         const days = []
@@ -34,35 +53,33 @@ const CreateTrip = (props) => {
     return (
         <main className='create-trip'>
             < section >
-                <h1>How are you today? Do you wanna plan a trip?</h1>
-                <div className='newtrip--form'>
-                    <form className='create-trip--form'>
+                <h1>Let`s plan a trip together!</h1>
+                <form className='create-trip--form'>
+                    <div className='input-container'>
+                        <label htmlFor="input">Where to go?</label>
+                        <input
+                            type="text"
+                            id="input"
+                            value={placeToGo}
+                            onChange={(e) => { setTripInfo({ ...tripInfo, placeToGo: e.target.value }) }}
+                            required
+                        />
+                    </div>
 
-                        <div className={`input-container ${placeToGo ? 'has-value' : ''}`}>
-                            <input
-                                type="text"
-                                id="input"
-                                value={placeToGo}
-                                onChange={(e) => { setTripInfo({ ...tripInfo, placeToGo: e.target.value }) }}
-                                required
-                            />
-                            <label htmlFor="input">Where to go?</label>
-                        </div>
-
-                        <div className={`input-container ${travelDays ? 'has-value' : ''}`}>
-                            <input
-                                type="text"
-                                id="input"
-                                value={travelDays}
-                                onChange={(e) => { setTripInfo({ ...tripInfo, travelDays: e.target.value }) }}
-                                required
-                            />
-                            <label htmlFor="input">How many days?</label>
-                        </div>
+                    <div className='input-container'>
+                        <label htmlFor="input">How many days?</label>
+                        <input
+                            type="number"
+                            id="input"
+                            value={travelDays}
+                            onChange={(e) => { setTripInfo({ ...tripInfo, travelDays: e.target.value }) }}
+                            required
+                        />
+                    </div>
 
 
-                    </form>
-                    {/* {placeToGo && travelDays &&
+                </form>
+                {/* {placeToGo && travelDays &&
                         <ButtonLink
                             onClick={handleNewTrip}
                             className={`main--button`}
@@ -70,26 +87,58 @@ const CreateTrip = (props) => {
                         ></ButtonLink>
 
                     } */}
-                </div>
             </section >
 
-            {travelDays && placeToGo &&
-                < section >
-                    <div className="create-trip--header">
-                        <h3 className="create-trip--travelDays"> {travelDays} days in </h3>
-                        <h1 className="create-trip--placeToGo"> {placeToGo}</h1>
-                    </div>
+            {
+                travelDays && placeToGo &&
+                <>
+                    < section className="create-trip--header" >
+                        <div className="create-trip--header-title">
+                            <h1 > {travelDays} days in </h1>
+                            <h1> {placeToGo}</h1>
+                        </div>
 
-                    {
-                        travelDays > 0 &&
-                        <div className="create-trip--itinerary">
-                            <h1>Itinerary</h1>
-                            {
-                                renderTripDays()
+                        <div className="create-trip--header-img">
+                            <input
+                                type="file"
+                                id="fileInput"
+                                style={{ display: 'none' }}
+                                onChange={handleHeaderImageChange}
+                                accept="image/*"
+                            />
+                            {tripInfo.headerImg ?
+                                <img src={tripInfo.headerImg} alt="Uploaded" />
+                                : <img src={logo} alt="placeholder logo"></img>}
+                        </div>
+                        <div className="create-trip--header-buttons">
+                            <ButtonLink
+                                onClick={handleAddImgButtonClick}
+                                className='main--button'
+                            >
+                                Upload Image
+                            </ButtonLink>
+                            {tripInfo.headerImg &&
+                                <ButtonLink
+                                    onClick={handleRemoveImgButtonClick}
+                                    className='main--button'
+                                >
+                                    Remove Image
+                                </ButtonLink>
                             }
                         </div>
-                    }
-                </section>
+                    </section>
+                    <section>
+                        {
+                            travelDays > 0 &&
+                            <div className="create-trip--itinerary">
+                                <h1>Itinerary</h1>
+                                {
+                                    renderTripDays()
+                                }
+                            </div>
+                        }
+                    </section>
+                </>
             }
         </main >
     )
