fde84e6eaba3761250d087a32dbeca4242ed8e43
bejabeja <miriam.abella211@gmail.com>
2024-05-20 09:49:27 +0200

    refactor: remove react striict mode from index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,14 +9,12 @@ import { FiltersProvider } from './context/filters';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <FiltersProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </FiltersProvider>
-    </AuthProvider>
-  </React.StrictMode >
+  <AuthProvider>
+    <FiltersProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FiltersProvider>
+  </AuthProvider>
 );
 
