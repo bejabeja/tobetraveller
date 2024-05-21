21939b865c726edc9945fc9e3bb83bd91a4da956
bejabeja <miriam.abella211@gmail.com>
2024-05-21 10:23:49 +0200

    create service to get favs

---
+++ client/src/services/getFavs.js
@@ -0,0 +1,16 @@
+export default function getFavs({ user_id }) {
+    return fetch(`${process.env.REACT_APP_API_URL}/favs?user_id=${user_id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => {
+            if (!res.ok) throw new Error('Response get favs is not ok');
+            return res.json();
+        })
+        .then(res => {
+            const { favs } = res.body;
+            return favs;
+        });
+}
