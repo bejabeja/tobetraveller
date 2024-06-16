362bc09fbbfbf26e7d897493259486e71a84b9ca
bejabeja <miriam.abella211@gmail.com>
2024-06-16 10:58:39 +0200

    extracted queries to repository user

---
+++ server/infrastructure/userRepository.js
@@ -0,0 +1,27 @@
+import client from '../routes/database.js';
+
+
+async function getUserBy(user_id) {
+    const { rows } = await client.query(
+        'SELECT * FROM users WHERE user_id = $1', [user_id]
+    );
+    return rows[0]
+}
+
+async function updateUserFavs(updatedCities, user_id) {
+    await client.query(
+        'UPDATE users SET favorite_cities = $1 WHERE user_id = $2',
+        [updatedCities, user_id]
+    );
+}
+
+async function getUserFavs(user_id) {
+    const { rows } = await client.query(
+        'SELECT favorite_cities FROM users WHERE user_id = $1', [user_id]
+    );
+
+    return rows[0].favorite_cities;
+
+}
+
+export { getUserBy, updateUserFavs, getUserFavs };
--- server/routes/favs.js
+++ server/routes/favs.js
@@ -1,19 +1,15 @@
 import { jsonResponse } from '../lib/jsonResponse.js';
 import express from 'express';
 const router = express.Router();
-import client from './database.js';
+import { getUserBy, updateUserFavs, getUserFavs } from '../infrastructure/userRepository.js';
 
 router.get('/', async (req, res) => {
-    console.log('API GETFAVS: ', req.query.user_id)
-
     try {
         const { user_id } = req.query;
-        const { rows } = await client.query(
-            'SELECT * FROM users WHERE user_id = $1', [user_id]
-        );
+        const user = await getUserBy(user_id);
         // user exists and has favorite cities
-        if (rows.length > 0 && rows[0].favorite_cities) {
-            const favoriteCities = rows[0].favorite_cities;
+        if (user.length > 0) {
+            const favoriteCities = user[0].favorite_cities;
             return res.status(200).json(
                 jsonResponse(
                     200,
@@ -42,29 +38,22 @@ router.post('/', async (req, res) => {
     try {
         const { city_id, user_id } = req.body;
 
-        const userExists = await client.query('SELECT 1 FROM users WHERE user_id = $1', [user_id]);
-        if (userExists.rows.length === 0) {
+        const user = await getUserBy(user_id);
+        if (!user) {
             return res.status(404).json(
                 jsonResponse(404, { message: 'User not found' })
             );
         }
 
-        const { rows } = await client.query(
-            'SELECT favorite_cities FROM users WHERE user_id = $1', [user_id]
-        );
 
-        const favoriteCities = rows[0].favorite_cities || [];
-        if (favoriteCities.includes(city_id)) {
+        if (user.favorite_cities.includes(city_id)) {
             return res.status(400).json(
                 jsonResponse(400, { message: 'City already exists in favorite cities' })
             );
         }
 
-        const updatedCities = [...favoriteCities, city_id];
-        await client.query(
-            'UPDATE users SET favorite_cities = $1 WHERE user_id = $2',
-            [updatedCities, user_id]
-        );
+        const updatedCities = [...user.favorite_cities, city_id];
+        await updateUserFavs(updatedCities, user_id)
 
         return res.status(201).json(
             jsonResponse(201, { favs: updatedCities })
@@ -82,17 +71,10 @@ router.delete('/', async (req, res) => {
     try {
         const { city_id, user_id } = req.body;
 
-        const { rows } = await client.query(
-            'SELECT favorite_cities FROM users WHERE user_id = $1', [user_id]
-        );
-
-        const favoriteCities = rows[0].favorite_cities;
+        const favoriteCities = await getUserFavs(user_id);
         const updatedCities = favoriteCities.filter(id => id !== city_id);
 
-        await client.query(
-            'UPDATE users SET favorite_cities = $1 WHERE user_id = $2',
-            [updatedCities, user_id]
-        );
+        await updateUserFavs(updatedCities, user_id);
 
         return res.status(200).json(
             jsonResponse(200, { favs: updatedCities })
