e7e77bb0ff9cad9569d5b322dbee054637e165de
bejabeja <miriam.abella211@gmail.com>
2025-01-30 13:38:56 +0100

    refactor, extract controller auth for signup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import { setToken } from '../repositories/authRepository.js';
-import { getUserByUsername } from '../repositories/userRepository.js';
-import { FIELDS_REQUIRED, USER_NOT_FOUND } from '../utils/constantsErrors.js';
+import { createNewUser, getUserByUsername } from '../repositories/userRepository.js';
+import { FIELDS_REQUIRED, INTERNAL_SERVER_ERROR, USER_NOT_FOUND } from '../utils/constantsErrors.js';
 import { jsonResponse } from '../utils/jsonResponse.js';
 import { generateAccessToken, generateRefreshToken } from '../utils/tokens.js';
 
@@ -58,5 +58,51 @@ export const login = async (req, res) => {
                 { error: USER_NOT_FOUND }
             )
         )
+    }
+}
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        return res.status(400).json(
+            jsonResponse(
+                400,
+                { message: FIELDS_REQUIRED }
+            )
+        );
+    }
+
+    const existingUser = await getUserByUsername(username);
+    if (existingUser) {
+        return res.status(400).json(
+            jsonResponse(
+                400,
+                { message: 'Username already exist' }
+            )
+        )
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        return res.status(400).json(
+            jsonResponse(
+                400,
+                { message: "Introduce a valid email" }
+            )
+        );
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await createNewUser(email, username, hashedPassword)
+        return res.status(200).json(
+            jsonResponse(
+                200,
+                { message: 'User successfully created' }
+            )
+        );
+    } catch (error) {
+        console.error('Error executing PostgreSQL query:', error);
+        return res.status(500).json({ message: INTERNAL_SERVER_ERROR });
     }
 }
--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,56 +1,8 @@
 import express from 'express';
-import { jsonResponse } from '../utils/jsonResponse.js';
-import bcrypt from 'bcrypt';
-import { getUserByUsername, createNewUser } from '../repositories/userRepository.js';
-import { INTERNAL_SERVER_ERROR, FIELDS_REQUIRED } from '../utils/constantsErrors.js';
+import { signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-        return res.status(400).json(
-            jsonResponse(
-                400,
-                { message: FIELDS_REQUIRED }
-            )
-        );
-    }
-
-    const existingUser = await getUserByUsername(username);
-    if (existingUser) {
-        return res.status(400).json(
-            jsonResponse(
-                400,
-                { message: 'Username already exist' }
-            )
-        )
-    }
-
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        return res.status(400).json(
-            jsonResponse(
-                400,
-                { message: "Introduce a valid email" }
-            )
-        );
-    }
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await createNewUser(email, username, hashedPassword)
-        return res.status(200).json(
-            jsonResponse(
-                200,
-                { message: 'User successfully created' }
-            )
-        );
-    } catch (error) {
-        console.error('Error executing PostgreSQL query:', error);
-        return res.status(500).json({ message: INTERNAL_SERVER_ERROR });
-    }
-
-});
+router.post('/', signup);
 
 export default router;
